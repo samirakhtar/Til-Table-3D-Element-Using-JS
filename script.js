1828612839aad4d8be2c441b230eb75719f75e73
MD Samir Akhtar <akhtarsamir242@gmail.com>
2021-06-01 22:24:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const box = document.querySelector('.container')
+const boxRect = box.getBoundingClientRect()
+
+box.addEventListener('mousemove',e=>{
+    const xPosition =(e.clientX - boxRect.left) / boxRect.width
+    const yPosition =(e.clientY - boxRect.top) / boxRect.height -0.6
+    const xOffset = -(xPosition - 0.6)
+    const dxNorm =Math.min(Math.max(xOffset, -0.6),0.6)
+    box.style.transform = `perspective(1000px)
+    box.style.transform = rotateY(${dxNorm*45}deg)
+    box.style.transform =  rotateX(${yPosition*45}deg) `
+})
+
+box.addEventListener('mouseleave', () => {
+    box.style.transform ='none'
+}
+)
